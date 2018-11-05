2c989110b24625845086ca87547191d00ca9e506
Mieszko Misztal <mistrzmiecho@hotmail.com>
2018-11-05 18:54:34 +0100

    Remove .NET

--- PortaCapena.Authentication.Core/Claims.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PortaCapena.Authentication.Core
-{
-    public static class Claims
-    {
-        public const string Role = "rls";
-        public const string UserId = "uid";
-    }
-}
